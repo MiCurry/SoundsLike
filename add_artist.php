b64e84d9473fff869d8293b095448d09c1b27e8c
Miles Curry <currymiles@gmail.com>
2013-05-20 13:18:48 -0600

    Update add_artist.php

--- add_artist.php
+++ add_artist.php
@@ -8,8 +8,8 @@
 <?php
     
     /* Connecting to ONID database */
-    $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "currymi-db",
-    "8CWoFsrAAJxoeCzj", "currymi-db");
+    $mysqli = new mysqli("DATABASE", "USERNAME",
+    "PASSWORD", "USERNAME");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     } else { echo"connected"; }  
